d01b1abe8b67db29012aec9cc84a7dbfca7384d1
cn14 <chethan.171co113@nitk.edu.in>
2020-07-22 11:05:27 +0530

    Updates

---
+++ precache-manifest.67eb0a6fe8e776a3a93661ab3d1212aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f541d340d6c0a6a70529c7fb9aa611d5",
+    "url": "/React-RandomUserMe/index.html"
+  },
+  {
+    "revision": "afc0fa9724f9fc2903f7",
+    "url": "/React-RandomUserMe/static/css/main.ffc9f13b.chunk.css"
+  },
+  {
+    "revision": "086a788f8fddd25d4768",
+    "url": "/React-RandomUserMe/static/js/2.174f8c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-RandomUserMe/static/js/2.174f8c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc0fa9724f9fc2903f7",
+    "url": "/React-RandomUserMe/static/js/main.2c5d360c.chunk.js"
+  },
+  {
+    "revision": "b9fa77a80b86ea98ab89",
+    "url": "/React-RandomUserMe/static/js/runtime-main.1c4b01b5.js"
+  }
+]);
